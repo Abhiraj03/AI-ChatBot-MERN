5a9ea9631b6549f9904420c67cb253591f6a61f7
Abhiraj03 <rajabhirajusa@gmail.com>
2025-07-07 12:31:53 -0700

    Added Login Route

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 export const getAllUsers = async (req:Request, res:Response, next:NextFunction) => {
     try {
@@ -15,9 +15,33 @@ export const getAllUsers = async (req:Request, res:Response, next:NextFunction) 
 export const userSignUp = async (req:Request, res:Response, next:NextFunction) => {
     try {
         const { name, email, password } = req.body;
+        const existingUser = await User.findOne({email:email});
+        if(existingUser) return res.status(401).send("User Already Registered!"); 
         const hashedPassword = await hash(password, 10);
         const user = new User({name, email, password: hashedPassword});
         await user.save();
+        return res.status(201).json({message:"OK", id:user._id.toString()});
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({message:"ERROR", cause:error.message});
+    }
+}
+
+
+export const userLogin = async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({email:email});
+        if(!user)
+        {
+            return res.status(401).send("User not registered!");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if(!isPasswordCorrect)
+        {   
+            return res.status(403).send("Incorrect Password...");
+        }
+
         return res.status(200).json({message:"OK", id:user._id.toString()});
     } catch (error) {
         console.log(error);
